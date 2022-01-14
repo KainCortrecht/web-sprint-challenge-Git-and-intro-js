be0aa83e489d5090f4de5da8008c09f8bb4c548e
Kain Cortrecht <kaincortrecht@Kains-MacBook-Air.local>
2022-01-14 16:02:08 -0500

    all done

--- index.js
+++ index.js
@@ -324,15 +324,21 @@ function lotsOfArt(array) {
 Use artistInfo to do the following: 
 1. Receive an array 
 2. Receive an artist's name
-3. return the artists bio
+3. Return the artists bio
 
 For example artistInfo(artists, 'Frida Kahlo') will return: 
   ["Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."]
 */
 function artistInfo(array, name){
-  return array[name].bio;
+  let filteredArray = [];
+  for(let i = 0; i < array.length; i++){
+    if( array[i].name === name){
+      filteredArray.push(array[i].bio)
+    }
+  }
+  return filteredArray;
 }
-
+//console.log(artistInfo(artists, 'Frida Kahlo'))
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 9: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use artistByCountry to do the following: 
@@ -345,14 +351,14 @@ For example artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 
 
 function artistByCountry(array, nationality){
   let place = [];
-  for (let i = 0; i <= artists.length-1; i++){
-    if(array[i] === nationality){
-      place.push(array[i].nationality);
+  for (let i = 0; i < artists.length; i++){
+    if(array[i].nationality === nationality){
+      place.push(array[i].name);
     }
   }
   return place;
 }
-//console.log(artistByCountry(artists, 'Spanish'));
+console.log(artistByCountry(artists, 'Spanish'));
 
 
 /* ***** END OF TASKS ***** */
